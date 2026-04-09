c254312777fdb4369ac5ff394d911e9b222f1d91
Luke Zhu <lzhu10@nuros.unl.edu>
2026-04-09 18:37:35 -0500

    update

--- part1/semaphores.c
+++ part1/semaphores.c
@@ -6,80 +6,78 @@
 
 #define CHAIRS 6
 #define NUM_CUSTOMERS 75
-#define STYLIST_SPEED 200000 // Time to cut hair
-#define CUSTOMER_ARRIVAL_GAP 50000
-#define SHOPPING_DELAY 500000 // Time a customer shops before returning
+#define DELAY 100000 // Adjust as needed for speed
 
-sem_t mutex;         // Protects the 'waiting' count
-sem_t stylistReady;  // Stylist waits for customer
-sem_t customerReady; // Customer waits for stylist
-sem_t cuttingDone;   // Customer waits for cut to finish
+sem_t mutex;
+sem_t stylistReady;
+sem_t customerReady;
 
 int waiting = 0;
-int total_haircuts = 0;
+int haircutsDone = 0;
 
-void *stylist_func(void *arg)
+void *stylist(void *arg)
 {
     while (1)
     {
-        // Wait for a customer to wake you up
+        // Stylist sleeps until a customer signals
         sem_wait(&customerReady);
 
         sem_wait(&mutex);
-        waiting--; // Customer leaves waiting chair to enter cutting chair
-        printf("[STYLIST]: Taking a customer. Chairs occupied: %d\n", waiting);
-        sem_post(&stylistReady); // Tell the customer to come to the chair
+        // Stylist takes a customer from a chair
+        waiting--;
+        sem_post(&stylistReady);
         sem_post(&mutex);
 
-        // Simulate the haircut
-        usleep(STYLIST_SPEED);
+        // Simulate the haircutting process
+        usleep(DELAY);
 
         sem_wait(&mutex);
-        total_haircuts++;
-        printf("[STYLIST]: Finished haircut #%d.\n", total_haircuts);
-        sem_post(&cuttingDone); // Signal the customer they can leave
+        haircutsDone++;
 
-        if (total_haircuts == NUM_CUSTOMERS)
+        // Exact output format requested
+        printf("Hair cut received\n");
+        printf("Number of haircuits done: %d\n", haircutsDone);
+        printf("Number of people waiting: %d\n", waiting);
+
+        if (haircutsDone == NUM_CUSTOMERS)
         {
             sem_post(&mutex);
             break;
         }
         sem_post(&mutex);
     }
-    printf("--- Stylist has finished for the day. ---\n");
     return NULL;
 }
 
-void *customer_func(void *arg)
+void *customer(void *arg)
 {
     int id = *(int *)arg;
-    int has_haircut = 0;
 
-    while (!has_haircut)
+    while (1)
     {
         sem_wait(&mutex);
 
         if (waiting < CHAIRS)
         {
             waiting++;
-            printf("Customer %2d: Entered salon. Waiting chairs: %d/%d\n", id, waiting, CHAIRS);
 
-            sem_post(&customerReady); // Wake stylist
+            // Exact output format requested
+            printf("Customer %d waiting\n", id);
+            printf("Number of people waiting: %d\n", waiting);
+
+            sem_post(&customerReady);
             sem_post(&mutex);
 
-            sem_wait(&stylistReady); // Wait for stylist to call you
-            printf("Customer %2d: Getting hair cut...\n", id);
-
-            sem_wait(&cuttingDone); // Wait for the stylist to finish
-            printf("Customer %2d: Finished and leaving salon.\n", id);
-            has_haircut = 1;
+            sem_wait(&stylistReady);
+            // Customer is now in the stylist's chair
+            break;
         }
         else
         {
-            // Salon is full
-            printf("Customer %2d: Salon full. Going shopping...\n", id);
+            // Optional: You can print if someone leaves,
+            // but your requested output doesn't show rejections.
             sem_post(&mutex);
-            usleep(SHOPPING_DELAY); // Wait before trying again
+            usleep(DELAY * 2); // Go shopping and come back
         }
     }
     return NULL;
@@ -91,19 +89,22 @@ int main()
     pthread_t customerThreads[NUM_CUSTOMERS];
     int ids[NUM_CUSTOMERS];
 
-    sem_init(&mutex, 1, 1);
-    sem_init(&stylistReady, 1, 0);
-    sem_init(&customerReady, 1, 0);
-    sem_init(&cuttingDone, 1, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&stylistReady, 0, 0);
+    sem_init(&customerReady, 0, 0);
 
-    pthread_create(&stylistThread, NULL, stylist_func, NULL);
+    pthread_create(&stylistThread, NULL, stylist, NULL);
 
     for (int i = 0; i < NUM_CUSTOMERS; i++)
     {
-        ids[i] = i + 1;
-        pthread_create(&customerThreads[i], NULL, customer_func, &ids[i]);
-        // Slight delay between arrivals to see the "build up"
-        usleep(CUSTOMER_ARRIVAL_GAP);
+        ids[i] = i; // Using 0-indexed IDs to match your sample
+        pthread_create(&customerThreads[i], NULL, customer, &ids[i]);
+
+        // Control arrival speed to see both build-up and empty states
+        if (i % 5 == 0)
+            usleep(DELAY * 2);
+        else
+            usleep(DELAY / 2);
     }
 
     for (int i = 0; i < NUM_CUSTOMERS; i++)
@@ -111,14 +112,11 @@ int main()
         pthread_join(customerThreads[i], NULL);
     }
 
-    // Join stylist last
     pthread_join(stylistThread, NULL);
 
     sem_destroy(&mutex);
     sem_destroy(&stylistReady);
     sem_destroy(&customerReady);
-    sem_destroy(&cuttingDone);
 
-    printf("Main: All 75 customers have been served. Program exiting.\n");
     return 0;
 }
